715064ba15ac153c382b0eda8962ed35211b330d
rajat <rajat110898@gmail.com>
2020-07-05 03:25:03 +0530

    loader added and minor css changes

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
-let app = document.getElementById('app');
+let app = document.getElementById("app");
 
-var typewriter = new Typewriter(app, {
-    loop: true
+$(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
 });
 
-typewriter.typeString('Hello World!')
-    .pauseFor(2000)
-    .deleteAll()
-    .typeString('I am a <strong>Computer Enthusiast.</strong>')
-    .pauseFor(2000)
-    .deleteChars(20)
-    .typeString('<strong>developer.</strong>')
-    .pauseFor(2000)
-    .deleteChars(10)
-    .typeString('<strong>designer.</strong>')
-    .pauseFor(2000)
-    .deleteChars(9)
-    .typeString('<strong>problem solver.</strong>')
-    .pauseFor(2000)
-    .start();
+var typewriter = new Typewriter(app, {
+  loop: true,
+});
+
+typewriter
+  .typeString("Hello World!")
+  .pauseFor(2000)
+  .deleteAll()
+  .typeString("I am a <strong>Computer Enthusiast.</strong>")
+  .pauseFor(2000)
+  .deleteChars(20)
+  .typeString("<strong>developer.</strong>")
+  .pauseFor(2000)
+  .deleteChars(10)
+  .typeString("<strong>designer.</strong>")
+  .pauseFor(2000)
+  .deleteChars(9)
+  .typeString("<strong>problem solver.</strong>")
+  .pauseFor(2000)
+  .start();
