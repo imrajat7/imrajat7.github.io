0ca7906f65020e167438201eb947b5389b514551
rajat <rajat110898@gmail.com>
2020-07-05 02:02:45 +0530

    first page added

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Hello World!')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('I am a <strong>Computer Enthusiast.</strong>')
+    .pauseFor(2000)
+    .deleteChars(20)
+    .typeString('<strong>developer.</strong>')
+    .pauseFor(2000)
+    .deleteChars(10)
+    .typeString('<strong>designer.</strong>')
+    .pauseFor(2000)
+    .deleteChars(9)
+    .typeString('<strong>problem solver.</strong>')
+    .pauseFor(2000)
+    .start();
