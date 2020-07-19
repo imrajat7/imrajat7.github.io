245f80d4b85c951795bc1ae28306a3fb2e72cb29
rajat <rajat110898@gmail.com>
2020-07-19 15:27:44 +0530

    skills section updated

--- js/script.js
+++ js/script.js
@@ -4,42 +4,6 @@ $(window).on("load", function () {
   $(".loader-wrapper").fadeOut("slow");
 });
 
-function myFunction(e) {
-  if (e.matches === true) {
-    $(function () {
-      $(".chart").easyPieChart({
-        scaleColor: "#ecf0f1",
-        lineWidth: 20,
-        lineCap: "butt",
-        barColor: "#1abc9c",
-        trackColor: "#ecf0f1",
-        size: 160,
-        animate: 800,
-      });
-    });
-  } else {
-    $(function () {
-      $(".chart").easyPieChart({
-        scaleColor: "#ecf0f1",
-        lineWidth: 20,
-        lineCap: "butt",
-        barColor: "#1abc9c",
-        trackColor: "#ecf0f1",
-        size: 160,
-        animate: 800,
-      });
-    });
-  }
-}
-
-let x;
-x = window.matchMedia("(max-width:700px)");
-window.onresize = function () {
-  if (window.innerWidth <= 700) x = window.matchMedia("(max-width:700px)");
-};
-myFunction(x);
-x.addEventListener("resize", myFunction);
-
 var typewriter = new Typewriter(app, {
   loop: true,
 });
